8c48e68151877fdd52929296218c47933dcc47a0
Santosh Mateti <matetisantosh37@gmail.com>
2026-07-05 08:43:34 +0000

    initial project strucutre

---
+++ scripts/system_info.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "========================================"
+echo " Linux Server Information"
+echo "========================================"
+
+echo
+echo "Date & Time:"
+date
+
+echo
+echo "Hostname:"
+hostname
+
+echo
+echo "Current User:"
+whoami
+
+echo
+echo "Kernel Version:"
+uname -r
+
+echo
+echo "System Uptime:"
+uptime
